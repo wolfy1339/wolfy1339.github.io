83fd64048b7edab90054ab1c5df9c195cefb1594
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-01-16 13:57:12 -0500

    Remove Cookie Clickers

--- cookieclicker/get_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-start /wait wget http://orteil.dashnet.org/cookieclicker/index.html
-start /wait wget -r -A '*.js' http://orteil.dashnet.org/cookieclicker/
-start /wait cd img && wget -r -A '*.png' http://://orteil.dashnet.org/cookieclicker/img
